b4f05ad396978dec5bf87f59967a5bb5b4ba46a2
Eric J. Smith <eric@codesmithtools.com>
2015-02-20 12:15:08 -0600

    Add RandomData code.

---
+++ Source/Tests/RandomDataTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Exceptionless.RandomData.Tests
+{
+    public class RandomDataTests
+    {
+        [Fact]
+        public void CanGenerateRandomData() {
+            
+        }
+    }
+}
